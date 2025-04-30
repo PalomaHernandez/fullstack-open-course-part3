fcef2c2688e1348555b0213f6a89bbab1d9cd6f0
PalomaHernandez <palohernandez01@gmail.com>
2025-04-29 21:52:30 -0300

    3.10 added frontend

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.static('dist'))
+
 var morgan = require('morgan')
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 
